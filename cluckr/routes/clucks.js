a8e5867d1625fd1eecb1499a246a15b89650179a
David Scott <david.r.scott91@gmail.com>
2018-02-05 12:24:34 -0800

    Created functionality to display posts in database

--- cluckr/routes/clucks.js
+++ cluckr/routes/clucks.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('clucks');
+  knex
+  .select()
+  .from('clucks')
+  .orderBy('created_at', 'DESC')
+  .then(clucks => {
+    res.render('clucks', {clucks: clucks});
+  });
 });
 
 router.get('/new', function(req, res, next) {
